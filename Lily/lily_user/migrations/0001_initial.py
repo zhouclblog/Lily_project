4253b3bed49c602279043c1b5d1d63f91e054c5a
xwp <xwp_fullstack@163.com>
2017-11-24 18:03:24 +0800

    fy版本一

--- Lily/lily_user/migrations/0001_initial.py
+++ Lily/lily_user/migrations/0001_initial.py
@@ -11,19 +11,40 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Address',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('is_delete', models.BooleanField(verbose_name='删除标记', default=False)),
+                ('update_time', models.DateTimeField(verbose_name='修改时间', auto_now_add=True)),
+                ('create_time', models.DateTimeField(verbose_name='创建时间', auto_now=True)),
+                ('recipient_name', models.CharField(verbose_name='收件人', max_length=20)),
+                ('recipient_addr', models.CharField(verbose_name='收件地址', max_length=256)),
+                ('recipient_phone', models.CharField(verbose_name='联系电话', max_length=11)),
+                ('is_default', models.BooleanField(verbose_name='是否默认', default=False)),
+            ],
+            options={
+                'db_table': 'address_info',
+            },
+        ),
+        migrations.CreateModel(
             name='UserInfo',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
-                ('is_delete', models.BooleanField(default=False, verbose_name='删除标记')),
-                ('update_time', models.DateTimeField(auto_now_add=True, verbose_name='修改时间')),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('is_delete', models.BooleanField(verbose_name='删除标记', default=False)),
+                ('update_time', models.DateTimeField(verbose_name='修改时间', auto_now_add=True)),
                 ('create_time', models.DateTimeField(verbose_name='创建时间', auto_now=True)),
-                ('username', models.CharField(max_length=30, verbose_name='用户名')),
-                ('password', models.CharField(max_length=30, verbose_name='密码')),
-                ('email', models.CharField(max_length=40, verbose_name='邮箱')),
-                ('is_activate', models.BooleanField(default=False, verbose_name='激活状态')),
+                ('username', models.CharField(verbose_name='用户名', max_length=30)),
+                ('password', models.CharField(verbose_name='密码', max_length=200)),
+                ('email', models.CharField(verbose_name='邮箱', max_length=40)),
+                ('is_activate', models.BooleanField(verbose_name='激活状态', default=False)),
             ],
             options={
                 'db_table': 'user_info',
             },
+        ),
+        migrations.AddField(
+            model_name='address',
+            name='userinfo',
+            field=models.ForeignKey(verbose_name='账户', to='lily_user.UserInfo'),
         ),
     ]
--- Lily/lily_user/models.py
+++ Lily/lily_user/models.py
@@ -29,7 +29,6 @@ class UserInfo(BaseModel):
     username = models.CharField(max_length=30, verbose_name="用户名")
     password = models.CharField(max_length=200, verbose_name="密码")
     email = models.CharField(max_length=40, verbose_name="邮箱")
-
     is_activate = models.BooleanField(default=False, verbose_name="激活状态")
 
     objects = UserInfoManager()
@@ -38,3 +37,59 @@ class UserInfo(BaseModel):
 
         db_table = "user_info"
 
+
+
+
+class AddressManager(models.Manager):
+    '''地址模型管理器类'''
+    def get_default_address(self, userinfo_id):
+          
+        '''查询指定用户的默认收货地址'''
+        '''
+        try:
+            addr = self.get(userinfo_id=userinfo_id, is_default=True)
+        except self.model.DoesNotExist:
+            addr = None
+
+        return addr
+        '''
+        pass
+
+
+    def add_one_address(self, userinfo_id, recipient_name, recipient_addr, recipient_phone):
+        '''添加收货地址'''
+        # 判断用户是否有默认收货地址
+        '''
+        addr = self.get_default_address(userinfo_id=userinfo_id)
+
+        if addr:
+            # 存在默认地址
+            is_default = False
+        else:
+            # 不存在默认地址
+            is_default = True
+
+        addr = self.create(userinfo_id=userinfo_id,
+                           recipient_name=recipient_name,
+                           recipient_addr=recipient_addr,
+                           recipient_phone=recipient_phone,
+                           is_default=is_default)
+
+        return addr
+        '''
+        pass
+
+
+
+class Address(BaseModel):
+    '''地址模型类'''
+    recipient_name = models.CharField(max_length=20, verbose_name='收件人')
+    recipient_addr = models.CharField(max_length=256, verbose_name='收件地址')
+    recipient_phone = models.CharField(max_length=11, verbose_name='联系电话')
+    is_default = models.BooleanField(default=False, verbose_name='是否默认')
+    userinfo = models.ForeignKey('UserInfo', verbose_name='账户')
+
+    objects = AddressManager()
+
+    class Meta:
+        db_table = 'address_info'
